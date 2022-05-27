02d0914fd65777ed303782b3e8f32e58a198780c
santhosh kumar <75415950+Santhoshkumar27@users.noreply.github.com>
2022-05-27 16:22:13 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyAlhxmFuVDRGJjvkFPSS5Q3PCyMiEFxJPw",
+  authDomain: "storytellin-72416.firebaseapp.com",
+  projectId: "storytellin-72416",
+  storageBucket: "storytellin-72416.appspot.com",
+  messagingSenderId: "993908358863",
+  appId: "1:993908358863:web:63708cad49a4b945ba41ba"
+};
